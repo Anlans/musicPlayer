c2a6ea82e18e6f4b3b1cd39b329d0dde78a1085b
honeyShi <918828350@qq.com>
2021-03-11 12:55:39 +0800

    第一次提交

---
+++ android/app/src/main/kotlin/com/webturing/musicplayer/musicplayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webturing.musicplayer.musicplayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:musicplayer/pages/splash/splash.dart';
+import 'package:musicplayer/pages/home/home.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'my first flutter',
+      routes: {
+        "/": (context)=>SplashPage(),
+        "/home": (context)=>HomePage(),
+      },
+      initialRoute: '/',
+    );
+
+  }
+}
