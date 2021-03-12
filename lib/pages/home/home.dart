bea5873ac3155b4fcc7bd652dcd4188ec0db2fd5
honeyShi <918828350@qq.com>
2021-03-12 19:17:20 +0800

    推荐列表(仅完成布局)

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,6 +6,7 @@ import 'package:musicplayer/pages/home/category.dart';
 import 'package:musicplayer/pages/square/square.dart';
 import 'package:musicplayer/widgets/song_list.dart';
 import 'package:musicplayer/widgets/song_list.dart';
+import 'package:musicplayer/widgets/recommend_list.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -23,18 +24,28 @@ class HomePage extends StatelessWidget {
             padding: EdgeInsets.only(top: screen.top+screen.calc(7)),
             child: Column(
               children: [
-                Header(),
-                MyBanner(),
-                Category(),
-                SongList(title: '你的歌单精选站', onMore: (){
-                  Navigator.pushNamed(context, '/square');
-                }, items: [
-                {'id': '43423', 'img': 'assets/tmp_cover_1.jpg', 'count': '9亿', 'title': '[华语新歌] 最新华语音乐专辑'},
-                {'id': '666', 'img': 'assets/tmp_cover_2.jpg', 'count': '34.9亿', 'title': '今天从<Titan>听起|私人雷达'},
-                {'id': '231', 'img': 'assets/tmp_cover_3.jpg', 'count': '5335', 'title': '[美语新歌]'},
-                {'id': '333', 'img': 'assets/tmp_cover_4.jpg', 'count': '19万', 'title': '[韩语新歌]'},
-                {'id': '788', 'img': 'assets/tmp_cover_5.jpg', 'count': '3232', 'title': '[日语新歌]'},
-                {'id': '344', 'img': 'assets/tmp_cover_6.jpg', 'count': '20亿亿', 'title': '[泰语新歌]'},
+                // Header(),
+                // MyBanner(),
+                // Category(),
+                //
+                // SongList(title: '你的歌单精选站', onMore: (){
+                //   Navigator.pushNamed(context, '/square');
+                // }, items: [
+                // {'id': '43423', 'img': 'assets/tmp_cover_1.jpg', 'count': '9亿', 'title': '[华语新歌] 最新华语音乐专辑'},
+                // {'id': '666', 'img': 'assets/tmp_cover_2.jpg', 'count': '34.9亿', 'title': '今天从<Titan>听起|私人雷达'},
+                // {'id': '231', 'img': 'assets/tmp_cover_3.jpg', 'count': '5335', 'title': '[美语新歌]'},
+                // {'id': '333', 'img': 'assets/tmp_cover_4.jpg', 'count': '19万', 'title': '[韩语新歌]'},
+                // {'id': '788', 'img': 'assets/tmp_cover_5.jpg', 'count': '3232', 'title': '[日语新歌]'},
+                // {'id': '344', 'img': 'assets/tmp_cover_6.jpg', 'count': '20亿亿', 'title': '[泰语新歌]'},
+                // ],),
+
+                RecommendList(title: '根据 Red 推荐', items: [
+                  {'id':'1111', 'img': 'assets/tmp_cover_1.jpg', 'title': 'Tempest', 'artist': 'Capo　Productions', 'tip':'眸中衍生星辰 于吾却尽是沙漠'},
+                  {'id':'1111', 'img': 'assets/tmp_cover_2.jpg', 'title': '岁月神偷', 'artist': '金玟岐', 'tip':'于吾却尽是沙漠'},
+                  {'id':'1111', 'img': 'assets/tmp_cover_3.jpg', 'title': '最美的期待', 'artist': '周笔畅', 'tip':'眸中衍生星辰 于吾却尽是沙漠'},
+                  {'id':'1111', 'img': 'assets/tmp_cover_4.jpg', 'title': 'Tempest', 'artist': 'Capo　Productions', 'tip':'眸中衍生星辰 于吾却尽是沙漠'},
+                  {'id':'1111', 'img': 'assets/tmp_cover_5.jpg', 'title': 'Tempest', 'artist': 'Capo　Productions', 'tip':'眸中衍生星辰 于吾却尽是沙漠'},
+                  {'id':'1111', 'img': 'assets/tmp_cover_6.jpg', 'title': 'Tempest', 'artist': 'Capo　Productions', 'tip':'眸中衍生星辰 于吾却尽是沙漠'},
                 ],),
               ],
             ),
---
+++ lib/widgets/recommend_list.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:musicplayer/util/screen_util.dart';
+
+
+typedef PlayCallBack=void Function(String);
+class RecommendList extends StatelessWidget{
+  final String title;
+  final VoidCallback onMore;
+  final List<Map> items;
+  final PlayCallBack onPlay;
+
+  RecommendList({Key key, this.title, this.onMore, this.items, this.onPlay}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen=Screen(context);
+
+    return Container(
+      margin: EdgeInsets.only(top: screen.calc(70)),
+      child: Column(
+        children: [
+
+          _Header(title: title, onMore: onMore),  //播放全部 头部
+
+          _Body(title: title, onMore: onMore,items: items,),   //播放头部所在下方的歌单排布
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class _Header extends StatelessWidget{
+  final String title;
+  final VoidCallback onMore;
+
+  _Header({Key key, this.title, this.onMore}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen=Screen(context);
+    return Container(
+      padding: EdgeInsets.only(left: screen.calc(32), right: screen.calc(32)),
+      height: screen.calc(52),
+      child: Row(
+        children: [
+          Center(
+            child: Text(title, style: TextStyle(
+              fontSize: screen.calc(34),
+              fontWeight: FontWeight.w700,
+            ),),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: onMore,
+            child: Container(
+              padding: EdgeInsets.only(left: screen.calc(24), right: screen.calc(24)),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xffe7e7e7), width: screen.calc(2)),
+                borderRadius: BorderRadius.circular(26),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.play_arrow),
+                  Text('播放全部'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class _Body extends StatelessWidget{
+  final List<Map> items;
+  final String title;
+  final VoidCallback onMore;
+  _Body({Key key, this.title, this.onMore, this.items}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen=Screen(context);
+
+    final columns=[];
+    while(items.length>0) {
+      columns.add(items.sublist(0,3));
+      items.removeRange(0, 3);
+    }
+
+    // print(columns);
+
+    return
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          padding: EdgeInsets.only(top: screen.calc(4), left: screen.calc(32)),
+          child: Row(
+            children: columns.map<Widget>((arr)=>Column(
+              children: arr.map<Widget>((data)=>_Item(data: data)).toList(),    //md, 逻辑完美
+            )).toList(),
+          ),
+        ),
+      );
+  }
+}
+
+//播放全部所在下方歌单 单个组件
+class _Item extends StatelessWidget{
+  final Map data;
+
+  _Item({Key key, this.data}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screen=Screen(context);
+
+    return Container(
+      width: screen.calc(641),
+      height: screen.calc(120),
+      
+      padding: EdgeInsets.only(top: screen.calc(20),),
+      margin: EdgeInsets.only(right: screen.calc(45)),
+      child: Row(
+        children: [
+          Container(
+            width: screen.calc(120),
+            height: screen.calc(120),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(data['img']),       //注意data的传参
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: screen.calc(20), top: screen.calc(16)),
+              alignment: Alignment.centerLeft,
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,//文字上下两行居左
+                children: [
+                  Text.rich(
+                   TextSpan(children: [
+                     TextSpan(text: '${data['title']}'),//传入歌名
+                     TextSpan(text: ' - ${data['artist']}', style: TextStyle(//传入歌手
+                       color: Color(0xff9a9a9a),
+                     )),
+                   ])
+                  ),
+
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: screen.calc(4), right: screen.calc(4)),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffff3a3a), width: 1),
+                          borderRadius: BorderRadius.circular(screen.calc(4)),
+                        ),
+                        child: Text('SQ', style: TextStyle(color: Color(0xffff3a3a), fontSize: screen.calc(20)),),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: screen.calc(8)),
+                        child: Text(data['tip'], style: TextStyle(color: Color(0xff9a9a9a)),),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Container(
+                height: screen.calc(50),
+                width: screen.calc(50),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffe6e6e6), width: screen.calc(2)),
+                  borderRadius: BorderRadius.circular(screen.calc(25)),
+                ),
+                child: Icon(Icons.play_arrow, size: screen.calc(30), color: Color(0xffff3a3a),),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
