4d76537bea54b14946fac7a2ff2ab4c73af2edfb
honeyShi <918828350@qq.com>
2021-03-17 17:07:40 +0800

    新增使用手势可以拖动slider

--- lib/pages/square/banner_slider.dart
+++ lib/pages/square/banner_slider.dart
@@ -25,43 +25,34 @@ class BannerSlider extends HookWidget{
       next=(_value.value+1)%items.length;
     // print('prev: $prev, cur: $cur, next: $next');
 
+
+
     final W=screen.calc(344),H=screen.calc(438),offsetH=screen.calc(200),offsetV=screen.calc(40);
 
-    Rect//方块位置
-      prevRect=Rect.fromLTWH((screen.width-W*0.8)/2-offsetH, offsetV, W*0.8, H*0.8),
-      curRect=Rect.fromLTWH((screen.width-W)/2, 0, W, H),
-      nextRect=Rect.fromLTWH((screen.width-W*0.8)/2+offsetH, offsetV, W*0.8, H*0.8),//next此处为+offset
-      otherRect=Rect.fromLTWH((screen.width-W)/2, 0, W, H);
+    final//存储opacity数据，scale数据用于滑动slider时的大小改变的数据
+      secondaryData={'scale': .8, 'opacity': .5},//prevData == nextData
+      curData={'scale': 1.0, 'opacity': 1.0},
+      otherData={'scale': .8, 'opacity': 0.0};
 
-    double
-      prevOpacity=0.5,
-      curOpacity=1.0,
-      nextOpacity=0.5,
-      otherOpacity=0.0;
 
     items.sort((item1, item2){
       // print('-----------------------');
       double opacity1, opacity2;
       // print('item1: $item1');
-      if(item1['index']==prev)
-        opacity1=prevOpacity;
+      if(item1['index']==prev || item1['index']==next)
+        opacity1=secondaryData['opacity'];
       else if(item1['index']==cur)
-        opacity1=curOpacity;
-      else if(item1['index']==next)
-        opacity1=nextOpacity;
-      else opacity1=otherOpacity;
+        opacity1=curData['opacity'];
+      else opacity1=otherData['opacity'];
 
       // print('opacity1: $opacity1');
-
       // print('item2: $item2');
-      if(item2['index']==prev)
-        opacity2=prevOpacity;
-      else if(item2['index']==cur)
-        opacity2=curOpacity;
-      else if(item2['index']==next)
-        opacity2=nextOpacity;
-      else opacity2=otherOpacity;
 
+      if(item2['index']==prev || item2['index']==next)
+        opacity2=secondaryData['opacity'];
+      else if(item2['index']==cur)
+        opacity2=curData['opacity'];
+      else opacity2=otherData['opacity'];
 
       // print('opacity2: $opacity2');
 
@@ -71,60 +62,62 @@ class BannerSlider extends HookWidget{
     });
 
     // print(items);
-
-    return Container(
-      // decoration: BoxDecoration(
-      //   color: Colors.blue,
-      // ),
-      margin: EdgeInsets.only(top: screen.calc(38)),
-      width: screen.width,
-      height: H,
-      child: Stack(//需要使用Position定位，用Stack方便
-        children: items.map((item){
-
-          Rect rect;
-          double opacity;
-          var index=item['index'];
-
-          if(index==cur) {
-            rect=curRect;
-            opacity=curOpacity;
+    return GestureDetector(
+      onHorizontalDragEnd: (DragEndDetails detail){
+        // print(detail.velocity.pixelsPerSecond.dx);//拖动速度快慢,左拖负
+        var dx=detail.velocity.pixelsPerSecond.dx;
+        if(dx.abs()>=50){//误触不理会
+          if(dx<0){//左拖
+            _value.value=(_value.value+1)%items.length;//下一个
+          }else{    //右拖
+            _value.value=(_value.value-1+items.length)%items.length;//上一个
           }
-          else if(index==prev) {
-            rect=prevRect;
-            opacity=prevOpacity;
-          }
-          else if(index==next) {
-            rect=nextRect;
-            opacity=nextOpacity;
-          }
-          else {
-            rect=otherRect;
-            opacity=otherOpacity;
-          }
+        }
+      },//横向拖曳
 
+      child: Container(
+        // decoration: BoxDecoration(
+        //   color: Colors.blue,
+        // ),
+        margin: EdgeInsets.only(top: screen.calc(38)),
+        height: H,
+        child: Stack(//需要使用Position定位，用Stack方便
+          children: items.map((item){
 
-          return Positioned(
-            left: rect.left,
-            top: rect.top,
+            var data;
+            var index=item['index'];
 
-            child: Opacity(
-              opacity: opacity,
-              child: Container(
-                width: rect.width,
-                height: rect.height,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-                child: Center(
-                  child: Text(item['title'], style: TextStyle(
-                    fontSize: 40,
-                  ),),
-                ),
-              ),
-            ),
-          );
-        }).toList(),
+            if(index==cur) data=curData;
+            else if(index==prev || index==next) data=secondaryData;
+            else data=otherData;
+
+            var offset=Offset(0, 0);
+            if(index==prev) offset=Offset(-offsetH, 0);
+            else if(index==next) offset=Offset(offsetH,0);
+
+            return Transform.translate(
+                offset: offset,
+                child: Transform.scale(
+                  scale: data['scale'],
+                  child: Opacity(
+                    opacity: data['opacity'],
+                    child: Container(
+                      width: W,
+                      height: H,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                      ),
+                      child: Center(
+                        child: Text(item['title'], style: TextStyle(
+                          fontSize: 40,
+                        ),),
+                      ),
+                    ),
+                  ),
+                )
+            );
+          }).toList(),
+        ),
       ),
     );
   }
